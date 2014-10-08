106cc25ce39db7970f643c5386633f01c696a05b
Lidia <lidiamcfreitas@gmail.com>
2014-10-08 01:16:10 +0100

    working animal

---
+++ Animal.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include "Animal.h"
+using namespace std;
+using namespace animal;
+
+int main(){
+    Animal lidia(20, "lidia");
+    Animal joao(21, "joao");
+    Animal unkown(1);    
+    cout << lidia << endl; 
+        
+
+}
